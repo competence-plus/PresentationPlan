9fba85ad1d2b17667b33f618be7e0feb2bcb1dfa
ESSARRAJ <essarraj.fouad@gmail.com>
2018-03-03 20:17:23 +0100

    Create Entities

---
+++ PresentationPlan/Entities/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationPlan.Entities
+{
+    public class Message
+    {
+        public string value { set; get; }
+
+        public PresrntaionPart PresrntaionPart { set; get; }
+    }
+}
